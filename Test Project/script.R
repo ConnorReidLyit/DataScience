0e7329d3813ff1a2cce68adfde82e0dbedb58e7b
ConnorReidLyit <L00010429@student.lyit.ie>
2018-04-12 11:27:21 +0100

    commit

--- Test Project/script.R
+++ Test Project/script.R
@@ -3,4 +3,18 @@ add_two_numbers <- function(no1, no2)
     result <- no1 + no2
     return(result)
 }
-add_two_numbers(3,5)
+add_two_numbers(3, 5)
+
+my_list <- list(a = matrix(1:9, 3), b = 1:5, c = matrix(1:4, 2), d = 2)
+sapply(my_list, sum)
+
+first_list <- list(a = matrix(1:9, 3), b = 1:5, c = matrix(1:4, 2), d = 2)
+second_list <- list(a = matrix(1:9, 3), b = 1:7, c = matrix(1:4, 2), d = 2)
+
+simple_function <- function(first_list, second_list)
+{
+    result <- NROW(first_list) + NROW(second_list)
+    return(result)
+}
+
+mapply(simple_function, first_list, second_list)
