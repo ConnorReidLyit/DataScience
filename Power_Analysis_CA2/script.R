611fc3ab9dfa1c4f9faf7bab69d62d44c35f7c92
ConnorReidLyit <L00010429@student.lyit.ie>
2018-05-07 22:10:25 +0100

    Final commit - updated crime category for a better story

--- Power_Analysis_CA2/script.R
+++ Power_Analysis_CA2/script.R
@@ -29,26 +29,27 @@ setwd("C:/Users/Connor/Documents/MSc Computing in Big Data Analytics/Data Scienc
 list.files()
 data <- read.csv("train.csv", header = TRUE)
 
-#create a new df from only the assault data
-assault_data <- subset(data, data$Category == 'ASSAULT')
-str(assault_data)
+levels(data$Category)
+#create a new df from only the 'SEX OFFENSES FORCIBLE' data
+data_of_interest <- subset(data, data$Category == 'SEX OFFENSES FORCIBLE')
+str(data_of_interest)
 
 #set the seed for reproducable results and add one to the number of 
 #records required in the event the the number of required records gets 
 #rounded down as its based on N from our analysis earlier
 set.seed(1);
-assault_data <- sample_n(assault_data, number_of_records$N + 1)
-str(assault_data)
+data_of_interest <- sample_n(data_of_interest, number_of_records$N + 1)
+str(data_of_interest)
 
 #create tabular view of the data to later apply the chi square test
-tbl_view <- table(assault_data$Category == 'ASSAULT', assault_data$PdDistrict)
+tbl_view <- table(data_of_interest$Category == 'SEX OFFENSES FORCIBLE', data_of_interest$PdDistrict)
 tbl_view
 
 # review the proportion of the the data in the table by row. 
 prop.table(tbl_view, 1)
 
-#apply the chi square test to the table containing the assault and District data
+#apply the chi square test to the table containing the Crime and District data
 stat_analysis_result <- chisq.test(tbl_view)
 
-# fail to reject H0 based on p-value of .18
+# reject H0 based on p-value of .0004124
 stat_analysis_result
